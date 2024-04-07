bd663abfa6331f67e1168066b1c1b22fa05409d5
bshahzad <y4794117b@iespoblenou.org>
2024-04-07 23:50:38 +0200

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,24 @@
+public class main {
+    public static void main(String[] args) {
+
+            String nombre = "Badar Shazad";
+            String titulo = "Tecnico de Informatico";
+            String email = "badar@gmail.com";
+            String telefono = "+34 123 456 789";
+            String experiencia = "Desarrollador en Java.";
+            String educacion = "Grado Superior en DAM, Institute Poblenou.";
+
+            System.out.println("Currículum Vitae");
+            System.out.println("----------------");
+            System.out.println("Nombre: " + nombre);
+            System.out.println("Título: " + titulo);
+            System.out.println("Email: " + email);
+            System.out.println("Teléfono: " + telefono);
+            System.out.println("\nExperiencia:");
+            System.out.println(experiencia);
+            System.out.println("\nEducación:");
+            System.out.println(educacion);
+
+            // java --module-path "D:\javafx-sdk-20.0.1\lib" --add-modules javafx.controls,javafx.fxml -jar Project.jar
+        }
+}
